175a41c216ae95a8a6d43c9bf6ba7c943ee6e520
dsteinke <dsteinke>
2025-04-02 17:16:49 +0300

    Added MappingProfile for Automapper

---
+++ TaskManager.API/Mapping/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using TaskManager.API.DTOs.User;
+using TaskManager.API.Models;
+
+namespace TaskManager.API.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<User, UserDTO>();
+            CreateMap<UserCreateDTO, User>();
+        }
+    }
+}
--- TaskManager.API/Program.cs
+++ TaskManager.API/Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.Data.Sqlite;
+using System.Data;
+using TaskManager.API.Interfaces.Repositories;
+using TaskManager.API.Interfaces.Services;
+using TaskManager.API.Mapping;
+using TaskManager.API.Repositories;
+using TaskManager.API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 // Add services to the container.
+builder.Services.AddSingleton<IDbConnection>(sp => new SqliteConnection(connectionString));
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+builder.Services.AddTransient<IUserService, UserService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 
 var app = builder.Build();
 
