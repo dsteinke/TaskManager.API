b6631454bdfc37c08331d3d4c38bd9b075ce8811
dsteinke <dsteinke>
2025-04-05 13:15:30 +0300

    Added Models for TaskManager

---
+++ TaskManager.API/Models/Priority.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskManager.API.Models
+{
+    public class Priority
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- TaskManager.API/Models/Task.cs
+++ TaskManager.API/Models/Task.cs
@@ -2,6 +2,18 @@
 {
     public class Task
     {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime? DueDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string CategoryId { get; set; }
+        public string PriorityId { get; set; }
+        public bool IsCompleted { get; set; }
 
+        public Category Category { get; set; }
+        public Priority Priority { get; set; }
     }
 }
