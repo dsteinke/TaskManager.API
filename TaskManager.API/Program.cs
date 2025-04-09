a8290f3d796a98d2fa6b7fa3f10af23fc5f85359
dsteinke <dsteinke>
2025-04-09 16:01:14 +0200

    Added ServiceExtensions class for program.cs

--- TaskManager.API/Program.cs
+++ TaskManager.API/Program.cs
@@ -3,11 +3,8 @@ using Microsoft.Data.Sqlite;
 using Microsoft.IdentityModel.Tokens;
 using System.Data;
 using System.Text;
-using TaskManager.API.Interfaces.Repositories;
-using TaskManager.API.Interfaces.Services;
+using TaskManager.API;
 using TaskManager.API.Mapping;
-using TaskManager.API.Repositories;
-using TaskManager.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,34 +12,18 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 
 // Add services to the container.
 builder.Services.AddSingleton<IDbConnection>(sp => new SqliteConnection(connectionString));
-builder.Services.AddTransient<IUserRepository, UserRepository>();
-builder.Services.AddTransient<IUserService, UserService>();
-builder.Services.AddTransient<IAuthService, AuthService>();
+builder.Services.ConfigureServices();
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            ValidAudience = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-        };
-    });
+// Add Authentication(Jwt)
+builder.Services.ConfigureAuthentication(builder.Configuration);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
