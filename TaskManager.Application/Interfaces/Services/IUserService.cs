daa5deb74864c28f0cc08f4aa2b1c902f280b327
dsteinke <dsteinke>
2025-04-13 22:04:13 +0200

    Refactored UserService

--- TaskManager.Application/Interfaces/Services/IUserService.cs
+++ TaskManager.Application/Interfaces/Services/IUserService.cs
@@ -5,11 +5,12 @@ namespace TaskManager.API.Interfaces.Services
     public interface IUserService
     {
         Task<bool> CreateUser(UserCreateDTO userDTO);
+        Guid GetUserId_LoggedInUser();
         Task<UserDTO> GetUserById(Guid userId);
         Task<UserDTO> GetUserByUsername(string username);
         Task<UserDTO> GetUserByEmail(string email);
         Task<List<UserDTO>> GetAllUsers();
-        Task<bool> UpdateUser(Guid userId, UserUpdateDTO userDTO);
-        Task<bool> DeleteUser(Guid userId);
+        Task<bool> UpdateUser(UserUpdateDTO userDTO);
+        Task<bool> DeleteUser();
     }
 }
